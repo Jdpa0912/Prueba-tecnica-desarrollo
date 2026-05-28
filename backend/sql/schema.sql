1ab03eb03bd5cab7941ee17f91ccd1771b2d875c
Jesus <jesusdparraa2004@gmail.com>
2026-05-27 22:59:22 -0500

    Nuevo schema con el campo delete_at

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 17.5
 -- Dumped by pg_dump version 17.5
 
--- Started on 2026-05-27 11:34:21
+-- Started on 2026-05-27 22:58:35
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -36,6 +36,7 @@ CREATE TABLE public.tasks (
     prioridad character varying(20) NOT NULL,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     user_id integer NOT NULL,
+    deleted_at timestamp without time zone,
     CONSTRAINT chk_estado CHECK (((estado)::text = ANY ((ARRAY['pendiente'::character varying, 'completado'::character varying])::text[]))),
     CONSTRAINT chk_prioridad CHECK (((prioridad)::text = ANY ((ARRAY['alta'::character varying, 'media'::character varying, 'baja'::character varying])::text[])))
 );
@@ -60,7 +61,7 @@ CREATE SEQUENCE public.tasks_id_seq
 ALTER SEQUENCE public.tasks_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4800 (class 0 OID 0)
+-- TOC entry 4802 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: tasks_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -77,7 +78,8 @@ CREATE TABLE public.users (
     id integer NOT NULL,
     email character varying(255) NOT NULL,
     password character varying(255) NOT NULL,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp without time zone
 );
 
 
@@ -100,7 +102,7 @@ CREATE SEQUENCE public.users_id_seq
 ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4801 (class 0 OID 0)
+-- TOC entry 4803 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -125,35 +127,35 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 4794 (class 0 OID 393973)
+-- TOC entry 4796 (class 0 OID 393973)
 -- Dependencies: 220
 -- Data for Name: tasks; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.tasks (id, titulo, descripcion, estado, prioridad, created_at, user_id) VALUES (1, 'Mi primera tarea', 'Esta es una tarea de prueba pero ya editada ', 'pendiente', 'alta', '2026-05-26 13:46:24.409674', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.tasks (id, titulo, descripcion, estado, prioridad, created_at, user_id) VALUES (4, 'Segunda', 'AAAAAAAAAASFFFFFFFFFFFFFFFFFFFFFFF', 'pendiente', 'baja', '2026-05-27 10:12:35.663986', 1) ON CONFLICT DO NOTHING;
+INSERT INTO public.tasks (id, titulo, descripcion, estado, prioridad, created_at, user_id, deleted_at) VALUES (1, 'Mi primera tarea', 'Esta es una tarea de prueba pero ya editada ', 'pendiente', 'alta', '2026-05-26 13:46:24.409674', 1, NULL);
+INSERT INTO public.tasks (id, titulo, descripcion, estado, prioridad, created_at, user_id, deleted_at) VALUES (4, 'Segunda', 'AAAAAAAAAASFFFFFFFFFFFFFFFFFFFFFFF', 'pendiente', 'baja', '2026-05-27 10:12:35.663986', 1, NULL);
 
 
 --
--- TOC entry 4792 (class 0 OID 393961)
+-- TOC entry 4794 (class 0 OID 393961)
 -- Dependencies: 218
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.users (id, email, password, created_at) VALUES (1, 'demo@gmail.com', '$2b$10$rMyQTkyxIazXwrEWDpKBbuAYcANk1enLw3n31/AXWBDsMvy0Ios96', '2026-05-26 13:46:24.409674') ON CONFLICT DO NOTHING;
+INSERT INTO public.users (id, email, password, created_at, deleted_at) VALUES (1, 'demo@gmail.com', '$2b$10$rMyQTkyxIazXwrEWDpKBbuAYcANk1enLw3n31/AXWBDsMvy0Ios96', '2026-05-26 13:46:24.409674', NULL);
 
 
 --
--- TOC entry 4802 (class 0 OID 0)
+-- TOC entry 4804 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: tasks_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.tasks_id_seq', 4, true);
+SELECT pg_catalog.setval('public.tasks_id_seq', 6, true);
 
 
 --
--- TOC entry 4803 (class 0 OID 0)
+-- TOC entry 4805 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -162,7 +164,7 @@ SELECT pg_catalog.setval('public.users_id_seq', 1, true);
 
 
 --
--- TOC entry 4644 (class 2606 OID 393984)
+-- TOC entry 4646 (class 2606 OID 393984)
 -- Name: tasks tasks_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -171,7 +173,7 @@ ALTER TABLE ONLY public.tasks
 
 
 --
--- TOC entry 4638 (class 2606 OID 393971)
+-- TOC entry 4639 (class 2606 OID 393971)
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -180,7 +182,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4640 (class 2606 OID 393969)
+-- TOC entry 4641 (class 2606 OID 393969)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -189,7 +191,15 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4641 (class 1259 OID 393991)
+-- TOC entry 4642 (class 1259 OID 393992)
+-- Name: idx_tasks_deleted_at; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_tasks_deleted_at ON public.tasks USING btree (deleted_at);
+
+
+--
+-- TOC entry 4643 (class 1259 OID 393991)
 -- Name: idx_tasks_estado; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -197,7 +207,7 @@ CREATE INDEX idx_tasks_estado ON public.tasks USING btree (estado);
 
 
 --
--- TOC entry 4642 (class 1259 OID 393990)
+-- TOC entry 4644 (class 1259 OID 393990)
 -- Name: idx_tasks_user_id; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -205,7 +215,15 @@ CREATE INDEX idx_tasks_user_id ON public.tasks USING btree (user_id);
 
 
 --
--- TOC entry 4645 (class 2606 OID 393985)
+-- TOC entry 4637 (class 1259 OID 393993)
+-- Name: idx_users_deleted_at; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_users_deleted_at ON public.users USING btree (deleted_at);
+
+
+--
+-- TOC entry 4647 (class 2606 OID 393985)
 -- Name: tasks tasks_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -213,7 +231,7 @@ ALTER TABLE ONLY public.tasks
     ADD CONSTRAINT tasks_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
 
 
--- Completed on 2026-05-27 11:34:21
+-- Completed on 2026-05-27 22:58:35
 
 --
 -- PostgreSQL database dump complete
